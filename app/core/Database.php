e21c66b88fc09ae851fbff07a29c2376d5f75a7a
priliapuspita04-stack <priliapuspita04.com>
2025-10-31 14:48:47 +0700

    ok

--- app/core/Database.php
+++ app/core/Database.php
@@ -5,9 +5,12 @@ class Database {
     
     private function __construct() {
         $config = require_once __DIR__ . '/../../config/database.php';
+
         
         try {
-            $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
+            $port = $config['port'] ?? 3306;
+            $dsn = "mysql:host={$config['host']};port={$port};dbname={$config['dbname']};charset={$config['charset']}";
+
             $this->conn = new PDO($dsn, $config['username'], $config['password']);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
